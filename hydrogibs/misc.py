7364419299d5960ef9799939fdb457dfbf87b3cc
giboul <axel.giboulot@epfl.ch>
2023-04-25 13:53:21 +0200

    Diviser pour mieux régner

--- hydrogibs/misc.py
+++ hydrogibs/misc.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 from scipy.optimize import OptimizeResult, minimize
 from warnings import warn
 
@@ -143,34 +142,6 @@ def specific_duration(S: np.ndarray) -> np.ndarray:
     return float(ds) if _float else ds
 
 
-class SoCoSe:
-
-    def __init__(self,
-                 S: float,
-                 L: float,
-                 Pa: float,
-                 P10: float,
-                 ta: float,
-                 Montana2: float,
-                 zeta: float = 1.0,
-                 tf: float = 5,
-                 dt: float = 0.01) -> None:
-
-        self.Pa = Pa
-        self.P10 = P10
-
-        self.ds = -0.69 + 0.32*np.log(S) + 2.2*np.sqrt(Pa/(P10*ta))
-        self.J = 260 + 21*np.log(S/L) - 54*np.sqrt(Pa/P10)
-
-        k = 24**Montana2/21*P10/(1 + np.sqrt(S)/(30*self.ds**(1/3)))
-        rho = 1 - 0.2 * self.J / (k * (1.25*self.ds)**(1-Montana2))
-        self.Q10 = zeta * k*S * rho**2 / ((15-12*rho)*(1.25*self.ds)**Montana2)
-
-        self.time = np.arange(0, tf, step=dt)
-        tau = 2*self.time/(3*self.ds)
-        self.Q = self.Q10 * 2 * tau**4 / (1 + tau**8)
-
-
 def crupedix(S: float, Pj10: float, R: float = 1.0):
     """
     Calculates the peak flow Q10 from a daily rain of 10 years return period.
@@ -189,132 +160,6 @@ def crupedix(S: float, Pj10: float, R: float = 1.0):
     return R * S**0.8 * (Pj10/80)**2
 
 
-class QdF:
-
-    """
-    Based on rainfall GradEx,
-    can estimate discharges for catchments of model type:
-        - Soyans
-        - Florac
-        - Vandenesse
-
-    Args:
-        model (str):          Either 'Soyans', 'Florac' or 'Vandenesse'
-        ds    (float) [h]:    Specific duration
-        S     (float) [km^2]: Catchment surface
-        L     (float) [km]:   Length of the thalweg
-        im    (float) [%]:    Mean slope of the thalweg
-
-    Calculates:
-        tc (float) [h]: concentration time
-    """
-
-    _coefs = dict(
-
-        soyans=dict(
-            A=(2.57, 4.86, 0),
-            B=(2.10, 2.10, 0.050),
-            C=(1.49, 0.660, 0.017)),
-
-        florac=dict(
-            A=(3.05, 3.53, 0),
-            B=(2.13, 2.96, 0.010),
-            C=(2.78, 1.77, 0.040)),
-
-        vandenesse=dict(
-            A=(3.970, 6.48, 0.010),
-            B=(1.910, 1.910, 0.097),
-            C=(3.674, 1.774, 0.013))
-    )
-
-    def __init__(self, model, ds, S, L, im) -> None:
-        """
-        Based on rainfall GradEx,
-        can estimate discharges for catchments of model type:
-            - Soyans
-            - Florac
-            - Vandenesse
-
-        Args:
-            model (str):          Either 'Soyans', 'Florac' or 'Vandenesse'
-            ds    (float) [h]:    Specific duration
-            S     (float) [km^2]: Catchment surface
-            L     (float) [km]:   Length of the thalweg
-            im    (float) [%]:    Mean slope of the thalweg
-
-        Calculates:
-            tc (float) [h]: concentration time
-        """
-        self.coefs = self._coefs[model]
-        self.ds = ds
-        self.im = im
-        self.S = S
-        self.L = L
-
-        self.tc = Turraza(S, L, im)
-
-    def _calc_coefs(self, a):
-        a1, a2, a3 = a
-        return 1/(a1*self._d/self.ds + a2) + a3
-
-    def discharge(self, d, T, Qsp, Q10):
-        """
-        Estimates the discharge for a certain flood duration
-        and a certain return period
-
-        Args:
-            d (numpy.ndarray) [h]: duration of the flood
-            T (numpy.ndarray) [y]: Return period
-            Qsp (numpy.ndarray): Specific discharge
-            Q10 (numpy.ndarray): Discharge for return period of 10 years
-
-        Returns:
-            (numpy.ndarray): Flood discharge
-        """
-
-        self._d = np.asarray(d)
-        Qsp = np.asarray(Qsp)
-        Q10 = np.asarray(Q10)
-        T = np.asarray(T)
-
-        self.A, self.B, self.C = map(
-            self._calc_coefs,
-            self.coefs.values()
-        )
-        return Q10 + Qsp * self.C * np.log(1 + self.A * (T-10)/(10*self.C))
-
-
-def rationalMethod(S: float,
-                   Cr: float,
-                   tc: float,
-                   ip: float = 1.0,
-                   dt: float = 0.01) -> tuple:
-    """
-    Computes a triangular hydrogram from a flood with volume Cr*tc*S
-
-    Args:
-        S (float): Catchemnt area
-        Cr (float): Peak runoff coefficient
-        tc (float): Concentration time
-        ip (float) [mm/h]: Rainfall intensity
-        dt (float): timestep, default to 1 if not specified
-
-    Returns:
-        time [h], discharge [m^3/s] (numpy.ndarray, numpy.ndarray)
-    """
-
-    q = Cr*ip*S
-    Qp = q/3.6
-
-    time = np.arange(0, 2*tc, step=dt)
-    Q = np.array([
-        Qp * t/tc if t < tc else Qp * (2 - t/tc)
-        for t in time
-    ])
-
-    return time, Q
-
-
 def zeller(montana_params: tuple,
            duration: float,
            vtime: float,  # TODO
@@ -327,12 +172,3 @@ def zeller(montana_params: tuple,
     if not np.isclose(vtime + rtime, duration, atol=atol):
         warn(f"\nt_v and t_r are not close enough")
     return Q
-
-
-def QdF_main():
-    qdf = QdF(model="soyans", ds=1, S=1.8, L=2, im=25)
-    Q10 = crupedix(S=1.8, Pj10=72, R=1.75)
-    d = np.linspace(0, 3)
-    Q100 = qdf.discharge(d, 100, Q10, Q10)
-    plt.plot(d, Q100)
-    plt.show()
