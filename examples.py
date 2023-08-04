d5ef529c754b2a6123b603f3d3bdc8b1e2b4e5ba
giboul <axel.giboulot@epfl.ch>
2023-08-04 22:20:50 +0200

    small edits

--- examples.py
+++ examples.py
@@ -3,14 +3,13 @@ from hydrogibs import crupedix
 import hydrogibs as hg
 import numpy as np
 from scipy.stats import skewnorm
-# plt.style.use("ggplot")
+from matplotlib import pyplot as plt
+plt.style.use("ggplot")
 
 
-# Q(T=10) according to Crupédix
 Q10 = crupedix(S=1.8, Pj10=72, R=1.0)
-# Q10 = 2.0
-print(f"{Q10  = :.2f} m3/s")
 
+# DEMO QDF method
 # Estimation of Q(T=100) according to the QdF method
 catchment = hg.QDF.Catchment(model="soyans", specific_duration=1)
 rain = hg.QDF.Rain(duration=1,
@@ -19,23 +18,16 @@ rain = hg.QDF.Rain(duration=1,
                    discharge_Q10=Q10,
                    dt=0.01)
 
-# QDF.App(rain=rain, catchment=catchment, style="ggplot")
 event = rain @ catchment
 
-# plt.plot(event.time, event.discharge)
-# plt.xlabel("Time [h]")
-# plt.ylabel("Discharge [m$^3$/s]")
-# plt.show()
+plt.plot(event.time, event.discharge)
+plt.title("QDF method")
+plt.xlabel("Time [h]")
+plt.ylabel("Discharge [m$^3$/s]")
+plt.show()
 
-# Laval
-X1 = 57.6/100  # [-] dR = X1*dP
-X2 = 7.28  # [mm] Interception par la végétation
-X3 = 2.4/100  # [h^-1] dH = X3*V*dt, V = (1-X1)*I*dt
-X4 = 0.38  # [h] temps de montée tm ≃ td
-
-t0 = 1  # h
+# DEMO GR4 method
 I0 = 66.7  # mm/h
-
 dt = 0.01
 time = np.arange(0, 24, step=dt)
 unit_rain = np.exp(-(time - 3)**2)
@@ -45,15 +37,14 @@ rain = hg.GR4.Rain(time, unit_rain * I0)
 
 catchment = hg.GR4.PresetCatchment("Rimbaud", surface=1.8)
 
-event = rain @ catchment
-
 hg.GR4.App(catchment, rain)
 
+# event = rain @ catchment
 # Qax, Pax, Vax = event.diagram(show=False).axes
-
 # Pax.set_title("Rimbaud")
 # plt.show()
 
+# DEMO rational_method
 durations = np.array((0.5, 1, 1.5, 2, 2.5))
 
 
@@ -63,14 +54,15 @@ def rainfall(d, T):
 
 for d in durations:
     t, Q = hg.rational_method(S=1.8, Cr=0.8, tc=d, ip=rainfall(d, 100)/d)
-    # plt.plot(t, Q, label=f"{d:.1f} h")
-# plt.xlabel("Temps (h)")
-# plt.ylabel("Débit (m$^3$/s)")
-# plt.legend(title="Durée (h)")
-# plt.show()
+    plt.plot(t, Q, label=f"{d:.1f} h")
+plt.title("Rational Method")
+plt.xlabel("Temps (h)")
+plt.ylabel("Débit (m$^3$/s)")
+plt.legend(title="Durée (h)")
+plt.show()
 
-
+# DEMO Ressaut
 Ressaut(
     q=10, i1=0.05, i2=0.002, p=0.5,
     h0=2, ms_K=50, x0=10, xt=20, dx=0.25
-)  # .diagram(show=True, figsize=(10, 3))
+).diagram(show=True, figsize=(10, 3))
