a5d7c906fdd3df4958fab6bf6a0d077db20532e1
giboul <axel.giboulot@epfl.ch>
2023-09-13 11:35:47 +0200

    yearly maxima

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="hydrogibs",
-    version="0.0.45",
+    version="0.0.47",
     description="A personal hydrology and hydraulics package"
                 " based on Christophe Ancey's teaching: "
                 "http://fr.ancey.ch/cours/masterGC/cours-hydraulique.pdf",
