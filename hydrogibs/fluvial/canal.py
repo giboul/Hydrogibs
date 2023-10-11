5e38a5b03534a71ddf1a9a8750c27e9d1e1c7cbc
giboul <axel.giboulot@epfl.ch>
2023-10-11 23:59:43 +0200

    adding clarity to docstring

--- hydrogibs/fluvial/canal.py
+++ hydrogibs/fluvial/canal.py
@@ -163,6 +163,7 @@ def polygon_properties(
       //\~~~/~~~\~~~~~~~~~//\
     _////\_// ↑ /\       ////\
     ///////// ↑ //\_____//////\
+    ///////// ↑ ///////////////\
               ↑
     This lenght should not contribute to the perimeter
 
