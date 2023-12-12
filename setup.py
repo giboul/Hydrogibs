b7139eac6213663ac6cc9b80f88634c004e8687d
giboul <axel.giboulot@epfl.ch>
2023-12-12 14:23:22 +0100

    yey

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="hydrogibs",
-    version="0.0.81",
+    version="0.0.82",
     description="A personal hydrology and hydraulics package"
                 " based on Christophe Ancey and Giovanni De Cesare's teaching: "
                 "http://fr.ancey.ch/cours/masterGC/cours-hydraulique.pdf",
